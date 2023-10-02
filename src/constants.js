d71f9d07eed9973bcf0c9e3aca0e67f66396dddd
himanshukrmr <himanshukrmr@gmail.com>
2023-10-02 15:24:37 +0530

    Completed the UI of sketch book app

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const COLORS = {
+    BLACK: 'black',
+    RED: 'red',
+    YELLOW: 'yellow',
+    GREEN: 'green',
+    BLUE: 'blue',
+    WHITE: 'white',
+    ORANGE: 'orange'
+}
+
+export {COLORS};
