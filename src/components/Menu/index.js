a833a417eece3827c13bdef074c5d47676158350
himanshukrmr <himanshukrmr@gmail.com>
2023-10-05 14:47:07 +0530

    added more redux logics for toolbox and menubar

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -9,12 +9,14 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./index.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { MENU_ITEMS } from "@/constants";
 import { menuitemClick } from "@/slice/menuSlice";
+import cx from "classnames";
 
 export default function Menu() {
   const dispatch = useDispatch();
+  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
   const handleMenuCLick = (itemName) => {
     dispatch(menuitemClick(itemName));
   };
@@ -22,13 +24,17 @@ export default function Menu() {
   return (
     <div className={styles.menuContainer}>
       <div
-        className={styles.iconWrapper}
+        className={cx(styles.iconWrapper, {
+          [styles.active]: activeMenuItem === MENU_ITEMS.PENCIL,
+        })}
         onClick={() => handleMenuCLick(MENU_ITEMS.PENCIL)}
       >
         <FontAwesomeIcon icon={faPencil} className={styles.icon} />
       </div>
       <div
-        className={styles.iconWrapper}
+       className={cx(styles.iconWrapper, {
+        [styles.active]: activeMenuItem === MENU_ITEMS.ERASER,
+      })}
         onClick={() => handleMenuCLick(MENU_ITEMS.ERASER)}
       >
         <FontAwesomeIcon icon={faEraser} className={styles.icon} />
--- src/components/ToolBox/index.js
+++ src/components/ToolBox/index.js
@@ -1,14 +1,27 @@
 import React from "react";
 import styles from "./index.module.css";
 import { COLORS, MENU_ITEMS } from "@/constants";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { changeBrushSize, changeColor } from "@/slice/toolBoxSlice";
+import cx from "classnames";
+
 
 const ToolBox = () => {
   const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
   const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL;
   const showBrushToolOption =
     activeMenuItem === MENU_ITEMS.ERASER || activeMenuItem == MENU_ITEMS.PENCIL;
-  const updateBrushSize = () => {};
+
+  const {color,size} = useSelector((state) => state.toolbox[activeMenuItem])
+  const dispatch = useDispatch();
+
+  const updateBrushSize = (e) => {
+    dispatch(changeBrushSize({ item: activeMenuItem, size: e.target.value }));
+  };
+
+  const updateColor = (newColor) => {
+    dispatch(changeColor({ item: activeMenuItem, color: newColor }));
+  };
 
   return (
     <div className={styles.toolBoxContainer}>
@@ -17,28 +30,34 @@ const ToolBox = () => {
           <h4 className={styles.toolText}>Stroke Color</h4>
           <div className={styles.itemContainer}>
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {[styles.active]: color === COLORS.BLACK })}
               style={{ backgroundColor: COLORS.BLACK }}
+              onClick={updateColor(COLORS.BLACK)}
             />
             <div
               className={styles.colorBox}
               style={{ backgroundColor: COLORS.BLUE }}
+              onClick={updateColor(COLORS.BLUE)}
             />
             <div
               className={styles.colorBox}
               style={{ backgroundColor: COLORS.RED }}
+              onClick={updateColor(COLORS.RED)}
             />
             <div
               className={styles.colorBox}
               style={{ backgroundColor: COLORS.ORANGE }}
+              onClick={updateColor(COLORS.ORANGE)}
             />
             <div
               className={styles.colorBox}
               style={{ backgroundColor: COLORS.GREEN }}
+              onClick={updateColor(COLORS.GREEN)}
             />
             <div
               className={styles.colorBox}
               style={{ backgroundColor: COLORS.YELLOW }}
+              onClick={updateColor(COLORS.YELLOW)}
             />
           </div>
         </div>
