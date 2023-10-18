5708379b7cbdc303b4680b494b9cb16e6ff8a30a
himanshukrmr <himanshukrmr@gmail.com>
2023-10-18 15:14:00 +0530

    increased the brush and eraser size

--- src/components/ToolBox/index.js
+++ src/components/ToolBox/index.js
@@ -36,7 +36,7 @@ const Toolbox = () => {
         {showBrushToolOption && <div className={styles.toolItem}>
             <h4 className={styles.toolText}>Brush Size</h4>
             <div className={styles.itemContainer}>
-                <input type="range" min={1} max={10} step={1} onChange={updateBrushSize} value={size}/>
+                <input type="range" min={1} max={30} step={1} onChange={updateBrushSize} value={size}/>
             </div>
         </div>}
     </div>)
